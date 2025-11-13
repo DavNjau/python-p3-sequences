c21c54d9964b3673361aa68cec3249836fd5d388
David <nnjui13@gmail.com>
2025-11-13 14:48:19 +0300

    Initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length <= 0:
+        return []
+    elif length == 1:
+        return [0]
+    
+    fib = [0, 1]
+    while len(fib) < length:
+        nextnumber = fib[-1] + fib[-2]
+        fib.append(nextnumber)
+    return fib
+
     pass
+
+
+
+
+
+
+
+    
+
